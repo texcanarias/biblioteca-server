4f212ff3a2308a3a14827801af902d206dec562d
texcanarias <antonio.tex@gmail.com>
2018-01-18 23:24:37 +0100

    Correciones para routes

--- application/controllers/cliente_1_0/Tipos.php
+++ application/controllers/cliente_1_0/Tipos.php
@@ -15,31 +15,18 @@ class Tipos extends Common {
         return $this->isAdministrador;
     }
 
-
     /**
      * Listado de los tipos de empresa 
      * Como respuesta se enviará un email con los datos de la nueva contraseña
      */
-    public function desbloquear_post() {
+    public function index_get() {
 
-        $ApiKey = $this->getApiKey();
-        $Id = $this->post("id");
 
-        $dao = $this->getDao();
-        $per = $dao->getPer();
-
-            $UsuarioPeticionarioWS = $per->getUsuarioPorApiKey($ApiKey);
-            $TipoUsuario = $UsuarioPeticionarioWS->getFtn_reg_tipo_usuario_Id();
-
-            if (serve\src\common\Niveles_acceso::$usuarioAdministrador == $TipoUsuario) {
-                $data = array();
-                $data[] = array("id" => "1", "tipo" => "Imprenta");
-                $data[] = array("id" => "2", "tipo" => "Serigrafía");
-                $data[] = array("id" => "3", "tipo" => "Repografía");
-                $this->set_response($data, \Restserver\Libraries\REST_Controller::HTTP_OK);
-            } else {
-                $data = array('code' => '1', 'error' => 'Permiso insuficientes', 'description' => 'No tiene permisos suficientes para esta acción.');
-                $this->set_response($data, \Restserver\Libraries\REST_Controller::HTTP_FORBIDDEN);
-            }
+        $data = array();
+        $data[] = array("id" => "1", "tipo" => "Imprenta");
+        $data[] = array("id" => "2", "tipo" => "Serigrafía");
+        $data[] = array("id" => "3", "tipo" => "Repografía");
+        $this->set_response($data, \Restserver\Libraries\REST_Controller::HTTP_OK);
     }
+
 }
