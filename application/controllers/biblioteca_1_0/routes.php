fe9da13e3ce35ca43de7b49da75cc32f6f1e5569
Antonio MTM casa <antonio.tex@gmail.com>
2018-03-22 18:36:45 +0100

    Cambios en las rutas

--- application/controllers/biblioteca_1_0/routes.php
+++ application/controllers/biblioteca_1_0/routes.php
@@ -9,7 +9,7 @@
   curl -i -X POST \
     -H "Content-Type: application/json" \
     -H "X-API-KEY: 21232f297a57a5a743894a0e4a801fc3" \
-    http://192.168.0.163/server/index.php/biblioteca_1_0 \
+    http://localhost/biblioteca/index.php/biblioteca_1_0 \
     -d '{"nombre":"EMpresa A","codigo":"2013","direccion":"Calle de arriba","ciudad":"BCN","provincia":"BCN","estado":"Spain","cp":"35012","persona_contacto":"Juan Diego","telefono":"666","movil":"666","fax":"666","email":"Enterprise A","url":"www.enterprise.com","comentarios":"comentarios"}'
 
     EDITAR EDICION PROVEEDOR
